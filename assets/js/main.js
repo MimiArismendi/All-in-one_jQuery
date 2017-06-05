84951671fab9c8f21b21e3377e4935c9b220c482
noemi <noemiarismendi.cc@gmail.com>
2017-06-05 13:38:16 -0500

    primera version creada con mapa

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+  });
+    }
+
+var cargarPagina = function(){
+    
+};
+
+$(document).ready(cargarPagina);
